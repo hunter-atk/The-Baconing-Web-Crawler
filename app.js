903e7020e63449421d6e94cb7862aefe65c5ba9a
Hunter Jefferson <hunterjefferson@Hunters-MacBook-Pro.local>
2018-06-11 11:57:15 -0500

    can get all movies for KB

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-'use strict';
-
-const PORT = process.env.PORT || 8080
 
 
-app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
-});
+const fetch = require('node-fetch')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+// const PORT = process.env.PORT || 8080
+
+fetch('https://www.imdb.com/name/nm0000102/')
+.then(data => data.text())
+.then(render);
+
+function render(htmlText){
+    const dom = new JSDOM(htmlText);
+    const movieContainer = dom.window.document.querySelector(".filmo-category-section")
+    const movies = movieContainer.querySelectorAll("b a")
+
+    for(const movie of movies){
+        console.log(movie.textContent);
+
+        //get all the movie links
+    }
+}
+
