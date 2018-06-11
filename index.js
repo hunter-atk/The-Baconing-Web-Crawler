55566e0a7a99f3ea857c11b29eb9c994f78fd63c
Chris Casey <cdcasey@gmail.com>
2018-06-11 13:53:52 -0500

    Get movies, get actors for each movie

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const fetch = require('node-fetch');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
+fetch('https://www.imdb.com/name/nm0000102/?ref_=fn_al_nm_1')
+  .then(data => data.text())
+  .then(render);
 
-fetch('https://www.imdb.com/name/nm0000102/?ref_=nv_sr_1')
-    .then(res => res.text())
-    .then(body => console.log(body));
+function render(htmlText) {
+  const dom = new JSDOM(htmlText);
+  const movieContainer = dom.window.document.querySelector(".filmo-category-section")
+  const movies = movieContainer.querySelectorAll('b a')
+
+  const movieLinks = []
+  for (const movie of movies) {
+    // console.log(`https://www.imdb.com${movie.getAttribute('href')}`);
+    // get all the movie links
+    const titleLink = movie.getAttribute('href').replace(/\?.+/, '');
+    movieLinks.push(`https://www.imdb.com${titleLink}`)
+  }
+  const cast = {}
+  for (const link of movieLinks) {
+    fetch(link)
+      .then(data => data.text())
+      .then((dataText) => {
+        console.log(link);
+        renderCast(dataText);
+      })
+  }
+  // console.log(movieLinks[0]);
+}
+
+function renderCast(htmlText) {
+  const dom = new JSDOM(htmlText);
+  const castContainer = dom.window.document.querySelector(".cast_list")
+  const cast = castContainer.querySelectorAll('td.itemprop > a')
+  console.log(cast.length);
+  // for (const castMember of cast) {
+  //   console.log(castMember.textContent);
+
+  // }
+
+}
